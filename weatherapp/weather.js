71db8439b74d96807d073ae66315456333ac44c7
Noah Msekni <msekninoah@gmail.com>
2023-09-24 14:18:02 +0200

    Fixed loading

--- weatherapp/weather.js
+++ weatherapp/weather.js
@@ -1,16 +1,19 @@
-// Function to show the loading component
 function showLoading() {
   const loading = document.getElementById("loading");
-  loading.classList.remove("hidden");
+  if (loading) {
+    loading.textContent = "Loading...";
+    loading.classList.remove("hidden");
+  }
 }
 
 // Function to hide the loading component
 function hideLoading() {
   const loading = document.getElementById("loading");
-  loading.classList.add("hidden");
+  if (loading) {
+    loading.classList.add("hidden");
+  }
 }
 
-// Function to fetch weather data and display it on the page
 async function getWeatherAndDisplay(apiKey, city) {
   showLoading();
 
@@ -25,39 +28,38 @@ async function getWeatherAndDisplay(apiKey, city) {
 
     const data = await response.json();
 
-    // Extract weather information
-    const weatherInfo = {
-      country: data.location.country,
-      cityName: data.location.name,
-      localtime: data.location.localtime,
-      icon: data.current.condition.icon,
-      condition: data.current.condition.text,
-      temperatureC: data.current.temp_c,
-      feelsLikeC: data.current.feelslike_c,
-      humidity: data.current.humidity,
-      uvIndex: data.current.uv,
-      windDirection: data.current.wind_dir,
-      windSpeedKph: data.current.wind_kph,
-    };
+    setTimeout(() => {
+      const weatherInfo = {
+        country: data.location.country,
+        cityName: data.location.name,
+        localtime: data.location.localtime,
+        icon: data.current.condition.icon,
+        condition: data.current.condition.text,
+        temperatureC: data.current.temp_c,
+        feelsLikeC: data.current.feelslike_c,
+        humidity: data.current.humidity,
+        uvIndex: data.current.uv,
+        windDirection: data.current.wind_dir,
+        windSpeedKph: data.current.wind_kph,
+      };
 
-    // Display weather information on the page
-    const weatherInfoContainer = document.getElementById("weatherInfo");
-    weatherInfoContainer.innerHTML = `
-      <h2>${weatherInfo.cityName}, ${weatherInfo.country}</h2>
-      <p>Local Time: ${weatherInfo.localtime}</p>
-      <img src="${weatherInfo.icon}" alt="${weatherInfo.condition}">
-      <p>Condition: ${weatherInfo.condition}</p>
-      <p>Temperature: ${weatherInfo.temperatureC}°C</p>
-      <p>Feels Like: ${weatherInfo.feelsLikeC}°C</p>
-      <p>Humidity: ${weatherInfo.humidity}%</p>
-      <p>UV Index: ${weatherInfo.uvIndex}</p>
-      <p>Wind: ${weatherInfo.windDirection} at ${weatherInfo.windSpeedKph} km/h</p>
-    `;
-
-    hideLoading();
+      // Display weather information on the page
+      const weatherInfoContainer = document.getElementById("weatherInfo");
+      weatherInfoContainer.innerHTML = `
+            <h2>${weatherInfo.cityName}, ${weatherInfo.country}</h2>
+            <p>Local Time: ${weatherInfo.localtime}</p>
+            <img src="${weatherInfo.icon}" alt="${weatherInfo.condition}">
+            <p>Condition: ${weatherInfo.condition}</p>
+            <p>Temperature: ${weatherInfo.temperatureC}°C</p>
+            <p>Feels Like: ${weatherInfo.feelsLikeC}°C</p>
+            <p>Humidity: ${weatherInfo.humidity}%</p>
+            <p>UV Index: ${weatherInfo.uvIndex}</p>
+            <p>Wind: ${weatherInfo.windDirection} at ${weatherInfo.windSpeedKph} km/h</p>
+          `;
+      hideLoading();
+    }, 1000);
   } catch (error) {
     console.error("Error:", error);
-    // Display an error message on the page if needed
     hideLoading();
   }
 }
@@ -65,21 +67,44 @@ async function getWeatherAndDisplay(apiKey, city) {
 // Add an event listener to the search button
 document.getElementById("searchButton").addEventListener("click", () => {
   const cityInput = document.getElementById("cityInput");
-  const city = cityInput.value.trim(); // Get and clean user input
+  const city = cityInput.value.trim();
   if (city) {
     const apiKey = "1f461a6b9ff94ec08be135215231709";
     getWeatherAndDisplay(apiKey, city);
   }
 });
 
-// Add an event listener for the "Enter" key in the input field
 document.getElementById("cityInput").addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     const cityInput = document.getElementById("cityInput");
-    const city = cityInput.value.trim(); // Get and clean user input
+    const city = cityInput.value.trim();
     if (city) {
       const apiKey = "1f461a6b9ff94ec08be135215231709";
       getWeatherAndDisplay(apiKey, city);
     }
   }
 });
+
+function updateHeaderLoadingText() {
+  const header = document.getElementById("Dots");
+  const originalText = header.textContent;
+  let dots = "";
+  let dotCount = 0;
+
+  const intervalId = setInterval(() => {
+    dots += ".";
+    dotCount++;
+
+    if (dotCount === 4) {
+      dotCount = 0;
+      dots = "";
+    }
+
+    const newText = originalText + dots;
+    header.textContent = newText;
+  }, 1000);
+
+  return intervalId;
+}
+
+const loadingInterval = updateHeaderLoadingText();
